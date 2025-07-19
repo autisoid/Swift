db5d316392deb5a62dd7c1b217ee66f0e4587723
autisoid <123592235+autisoid@users.noreply.github.com>
2025-07-19 22:42:43 +0100

    initial commit

---
+++ src/minecraft/team/swift/features/module/modules/player/NoFall.java
@@ -0,0 +1,26 @@
+package team.swift.features.module.modules.player;
+
+import team.swift.event.EventTarget;
+import team.swift.event.events.impl.SendPacketEvent;
+import team.swift.event.events.impl.UpdateEvent;
+import team.swift.features.module.Module;
+import team.swift.features.module.ModuleCategory;
+import net.minecraft.network.play.client.CPacketPlayer;
+import org.lwjgl.input.Keyboard;
+import team.swift.settings.Setting;
+
+/* Created by xWhitey on 05.05.2021 */
+public class NoFall extends Module {
+    public Setting<String> mode = new Setting<String>("Mode", "Mode", this, "Packet", "Packet");
+
+    public NoFall() {
+        super("NoFall", ModuleCategory.PLAYER, Keyboard.KEY_N);
+    }
+
+    @EventTarget
+    public void onUpdate(UpdateEvent event) {
+        if (mc.player.fallDistance > 2) {
+            mc.player.connection.sendPacket(new CPacketPlayer(true));
+        }
+    }
+}
